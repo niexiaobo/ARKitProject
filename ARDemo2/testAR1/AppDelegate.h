c71a3c5e46de202f073b31b37bcc353108baaace
flash_lifing@163.com <flash_lifing@163.com>
2018-05-17 15:49:57 +0800

    添加demo

---
+++ ARDemo2/testAR1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testAR1
+//
+//  Created by niexiaobo on 2018/5/17.
+//  Copyright © 2018年 NXB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARDemo2/testAR1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testAR1
+//
+//  Created by niexiaobo on 2018/5/17.
+//  Copyright © 2018年 NXB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
